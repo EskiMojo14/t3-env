cc779c40ebdae09d2fc2154630391ae6626831d8
EskiMojo14 <ben.j.durrant@gmail.com>
2025-01-29 01:31:31 +0000

    rename to be closer to original naming

--- packages/core/test/smoke.test.ts
+++ packages/core/test/smoke.test.ts
@@ -1,19 +1,19 @@
 import type { TestOptions } from "bun:test";
-import { describe, expect, test } from "bun:test";
+import { describe as bunDescribe, test as bunTest, expect } from "bun:test";
 import { expectTypeOf } from "expect-type";
 import type { StandardSchemaDictionary } from "../src";
 import { createEnv } from "../src";
 
-const defineTest =
+const test =
   <Opts>(
     name: string,
     cb: (opts: Opts) => void,
     testOptions?: number | TestOptions,
   ) =>
   (opts: Opts) =>
-    test(name, () => cb(opts), testOptions);
+    bunTest(name, () => cb(opts), testOptions);
 
-const defineTestGroup =
+const describe =
   <OptMap extends Record<string, unknown>>(
     groupName: string,
     tests: {
@@ -21,97 +21,85 @@ const defineTestGroup =
     },
   ) =>
   (optsMap: OptMap) => {
-    describe(groupName, () => {
+    bunDescribe(groupName, () => {
       for (const [name, test] of Object.entries(tests)) {
         test(optsMap[name]);
       }
     });
   };
 
-export const returnType = defineTestGroup("return type is correctly inferred", {
-  simple: defineTest(
-    "simple",
-    (opts: {
-      server: StandardSchemaDictionary.Matching<{ BAR: string }>;
-      client: StandardSchemaDictionary.Matching<{ FOO_BAR: string }>;
-    }) => {
-      const env = createEnv({
-        clientPrefix: "FOO_",
-        ...opts,
-        runtimeEnvStrict: {
-          BAR: "bar",
-          FOO_BAR: "foo",
-        },
-      });
-
-      expectTypeOf(env).toEqualTypeOf<
-        Readonly<{
-          BAR: string;
-          FOO_BAR: string;
-        }>
-      >();
-
-      expect(env).toMatchObject({
+export const returnType = describe("return type is correctly inferred", {
+  simple: test("simple", (opts: {
+    server: StandardSchemaDictionary.Matching<{ BAR: string }>;
+    client: StandardSchemaDictionary.Matching<{ FOO_BAR: string }>;
+  }) => {
+    const env = createEnv({
+      clientPrefix: "FOO_",
+      ...opts,
+      runtimeEnvStrict: {
         BAR: "bar",
         FOO_BAR: "foo",
-      });
-    },
-  ),
+      },
+    });
 
-  withTransforms: defineTest(
-    "with transforms",
-    (opts: {
-      server: StandardSchemaDictionary.Matching<
-        { BAR: string },
-        { BAR: number }
-      >;
-      client: StandardSchemaDictionary.Matching<{ FOO_BAR: string }>;
-    }) => {
-      const env = createEnv({
-        clientPrefix: "FOO_",
-        ...opts,
-        runtimeEnvStrict: {
-          BAR: "123",
-          FOO_BAR: "foo",
-        },
-      });
-      expectTypeOf(env).toEqualTypeOf<
-        Readonly<{
-          BAR: number;
-          FOO_BAR: string;
-        }>
-      >();
+    expectTypeOf(env).toEqualTypeOf<
+      Readonly<{
+        BAR: string;
+        FOO_BAR: string;
+      }>
+    >();
 
-      expect(env).toMatchObject({
-        BAR: 123,
+    expect(env).toMatchObject({
+      BAR: "bar",
+      FOO_BAR: "foo",
+    });
+  }),
+
+  withTransforms: test("with transforms", (opts: {
+    server: StandardSchemaDictionary.Matching<{ BAR: string }, { BAR: number }>;
+    client: StandardSchemaDictionary.Matching<{ FOO_BAR: string }>;
+  }) => {
+    const env = createEnv({
+      clientPrefix: "FOO_",
+      ...opts,
+      runtimeEnvStrict: {
+        BAR: "123",
         FOO_BAR: "foo",
-      });
-    },
-  ),
+      },
+    });
+    expectTypeOf(env).toEqualTypeOf<
+      Readonly<{
+        BAR: number;
+        FOO_BAR: string;
+      }>
+    >();
 
-  withoutClientVars: defineTest(
-    "without client vars",
-    (opts: {
-      server: StandardSchemaDictionary.Matching<{ BAR: string }>;
-    }) => {
-      const env = createEnv({
-        clientPrefix: "FOO_",
-        ...opts,
-        client: {},
-        runtimeEnvStrict: {
-          BAR: "bar",
-        },
-      });
+    expect(env).toMatchObject({
+      BAR: 123,
+      FOO_BAR: "foo",
+    });
+  }),
 
-      expectTypeOf(env).toEqualTypeOf<
-        Readonly<{
-          BAR: string;
-        }>
-      >();
-
-      expect(env).toMatchObject({
+  withoutClientVars: test("without client vars", (opts: {
+    server: StandardSchemaDictionary.Matching<{ BAR: string }>;
+  }) => {
+    const env = createEnv({
+      clientPrefix: "FOO_",
+      ...opts,
+      client: {},
+      runtimeEnvStrict: {
         BAR: "bar",
-      });
-    },
-  ),
+      },
+    });
+
+    expectTypeOf(env).toEqualTypeOf<
+      Readonly<{
+        BAR: string;
+      }>
+    >();
+
+    expect(env).toMatchObject({
+      BAR: "bar",
+    });
+  }),
 });
